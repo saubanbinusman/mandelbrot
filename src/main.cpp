351e541427398331ff3f86fddb9c94bf21971347
Sauban bin Usman <saubanbinusman@yahoo.com>
2017-10-28 21:53:38 +0500

    Playing around with SDL, currently shows a circle being drawn

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
+#include <cmath>
+
 #include <SDL2/SDL.h>
 
 using namespace std;
 
 // Screen dimensions
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_WIDTH = 500;
+const int SCREEN_HEIGHT = 500;
 
 int main(int argc, char** args)
 {
@@ -13,7 +15,7 @@ int main(int argc, char** args)
 	SDL_Window* window = NULL;
 	
 	// The surface contained by the window
-	SDL_Surface* screenSurface = NULL;
+	//SDL_Surface* screenSurface = NULL;
 	
 	// Initialize SDL
 	if (SDL_Init(SDL_INIT_VIDEO) == -1)
@@ -32,23 +34,84 @@ int main(int argc, char** args)
 		return 0;
 	}
 	
-	screenSurface = SDL_GetWindowSurface(window);
+	//screenSurface = SDL_GetWindowSurface(window);
 	
 	// Fill the surface white
-	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface -> format, 0xFF, 0xFF, 0xFF));
+	//SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface -> format, 0xFF, 0xFF, 0xFF));
 	
 	// Update the surface
-	SDL_UpdateWindowSurface(window);
+	//SDL_UpdateWindowSurface(window);
 	
-	SDL_Delay(2000);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 	
-	
-	// Game Loop
-	while (true)
+	if (renderer == NULL)
 	{
-		
+		cout << "Could not create Renderer, Error: " << SDL_GetError() << endl;
+		return 0;
 	}
 	
+	SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); // White
+	SDL_RenderClear(renderer); // Fil it with white
+	
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // Black
+	
+	/*
+	for (int i = 0; i < SCREEN_WIDTH; i += 1)
+	{
+		SDL_RenderDrawPoint(renderer, i, i);
+	}
+	
+	for (int i = SCREEN_WIDTH, j = 0; i >= 0; i -= 1, j += 1)
+	{
+		SDL_RenderDrawPoint(renderer, i, j);
+	}
+	
+	for (int i = 0; i < SCREEN_WIDTH; i++)
+	{
+		SDL_RenderDrawPoint(renderer, i, SCREEN_HEIGHT >> 1);
+	}
+	
+	for (int i = 0; i < SCREEN_HEIGHT; i++)
+	{
+		SDL_RenderDrawPoint(renderer, SCREEN_WIDTH >> 1, i);
+	}*/
+	
+	//const double PI = 3.14159265358979323846;
+	
+	for (double x = -1.0; x <= 1.0; x += 0.001)
+	{
+		double y = sqrt(1.0 - (x * x));
+		
+		int xx = (int)(x * 100) + (SCREEN_WIDTH >> 1);
+		int yy = (int)(y * 100);
+		
+		//cout << "(" << xx << ", " << yy << ")" << endl;
+		
+		//double x_Rad = (double)(x) * (PI / 180.0);
+		
+		//int y = (int)(sin(x_Rad) * 45) + (SCREEN_WIDTH >> 1);
+		
+		//int y = (int)((sin((double)x)) + (SCREEN_HEIGHT >> 1));
+		
+		//int xx = (x < 180) ? x : -x + (SCREEN_WIDTH);
+		
+		SDL_RenderDrawPoint(renderer, xx, (SCREEN_WIDTH >> 1) + yy);
+		SDL_RenderDrawPoint(renderer, xx, (SCREEN_WIDTH >> 1) - yy);
+		SDL_RenderPresent(renderer);
+		SDL_Delay(1);
+	}
+	
+	SDL_RenderPresent(renderer);
+	
+	SDL_DestroyRenderer(renderer);
+	
+	// Delay to allow the user to see the window
+	// otherwise, it just flashes and closes
+	SDL_Delay(3000);
+	
+	// Free up memory
+	SDL_DestroyWindow(window);
+	SDL_Quit();
 	
 	return 0;
 }
